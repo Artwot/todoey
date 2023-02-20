1192f4777df52fc9418c13ce63433797ff51aab7
Artwot <jsarabiar98@gmail.com>
2023-02-20 14:19:00 -0600

    Add ColorScheme with custom colors

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'screens/tasks_manager.dart';
+import 'screens/tasks_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,8 +11,47 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: TasksManager(),
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: _colorSchemeLightM3,
+        useMaterial3: true,
+      ),
+      home: const TasksScreen(),
     );
   }
 }
+
+const ColorScheme _colorSchemeLightM3 = ColorScheme(
+  brightness: Brightness.light,
+  primary: Color(0xFF3C6657),
+  onPrimary: Color(0xFFFFFFFF),
+  primaryContainer: Color(0xFFBEEDD9),
+  onPrimaryContainer: Color(0xFF3C6657),
+  secondary: Color(0xFF625B71),
+  onSecondary: Color(0xFFFFFFFF),
+  secondaryContainer: Color(0xFFE8DEF8),
+  onSecondaryContainer: Color(0xFF1D192B),
+  tertiary: Color(0xFF7D5260),
+  onTertiary: Color(0xFFFFFFFF),
+  tertiaryContainer: Color(0xFFFFD8E4),
+  onTertiaryContainer: Color(0xFF31111D),
+  error: Color(0xFFB3261E),
+  onError: Color(0xFFFFFFFF),
+  errorContainer: Color(0xFFF9DEDC),
+  onErrorContainer: Color(0xFF410E0B),
+  background: Color(0xFF3C6657),
+  onBackground: Color(0xFF1C1B1F),
+  surface: Color(0xFFFFFBFE),
+  onSurface: Color(0xFF1C1B1F),
+  surfaceVariant: Color(0xFFE7E0EC),
+  onSurfaceVariant: Color(0xFF49454F),
+  outline: Color(0xFF79747E),
+  outlineVariant: Color(0xFFCAC4D0),
+  shadow: Color(0xFF000000),
+  scrim: Color(0xFF000000),
+  inverseSurface: Color(0xFF313033),
+  onInverseSurface: Color(0xFFF4EFF4),
+  inversePrimary: Color(0xFFD0BCFF),
+  // The surfaceTint color is set to the same color as the primary.
+  surfaceTint: Color(0xFF3C6657),
+);
