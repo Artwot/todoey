f93854c672d57ffa5aae2bfd250c73ac9b1fe2bb
Artwot <jsarabiar98@gmail.com>
2023-02-19 22:54:42 -0600

    Add TasksManager screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'screens/tasks_manager.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -10,11 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World'),
-        ),
-      ),
+      home: TasksManager(),
     );
   }
 }
---
+++ lib/screens/tasks_manager.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TasksManager extends StatelessWidget {
+  const TasksManager({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
